8f668a355d88167da7ecf1fffb99c03afdfc8549
Thomas Buchwald <thomas.buchwald@tu-freiberg.de>
2023-03-28 14:11:37 +0200

    included lots of GPT4 advice in main models.
    - verbose plural name "staff"
    - some help_text on unclear fields
    - __str__ methods in all models
    - related_name for most ForeignKeys
    - parent sample is now a ForeignKey on self

--- main/models.py
+++ main/models.py
@@ -4,39 +4,67 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 class Institute(models.Model):
     name = models.CharField(max_length=255)
-    affiliation = models.CharField(max_length=255)
+    affiliation = models.CharField(max_length=255, null=True, blank=True,
+                  help_text="if applicable: umbrella organization")
     street = models.CharField(max_length=50)
     postcode = models.CharField(max_length=10)
     city = models.CharField(max_length=50)
-    telephone = models.CharField(max_length=50)
-    email = models.EmailField()
+    # could in the future be changed to PhoneNumberField
+    telephone = models.CharField(max_length=50,
+                help_text="e.g. administration office")
+    email = models.EmailField(help_text="e.g. administration office")
     date_registered = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Staff(models.Model):
-    institute = models.ForeignKey(Institute, on_delete=models.PROTECT)
+    institute = models.ForeignKey(Institute, on_delete=models.PROTECT,
+                related_name="staff_members")
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     # birthday is not added for privacy reasons; uniqueness is guaranteed by email
     # birthday = models.DateField()
     email = models.EmailField(unique=True)
+    # could in the future be changed to PhoneNumberField
     telephone = models.CharField(max_length=50)
     ACTIVE_CHOICES = [(True, 'Active'), (False, 'Inactive')]
-    active = models.BooleanField(choices=ACTIVE_CHOICES, default=False)
+    active = models.BooleanField(choices=ACTIVE_CHOICES, default=False,
+             help_text="employment status")
     date_registered = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        verbose_name_plural = "Staff"
+
 class User(models.Model):
+    # this information is set by Django or Django admin, no user input required
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     date_registered = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Method(models.Model):
-    institute = models.ForeignKey(Institute, on_delete=models.PROTECT)
+    institute = models.ForeignKey(Institute, on_delete=models.PROTECT,
+                related_name="methods")
     name = models.CharField(max_length=255)
-    method_folder = models.CharField(max_length=255, null=True)
+    # if additional information for the method is supplied, it will be stored in method_folder
+    method_folder = models.CharField(max_length=255, null=True, blank=True)
     date_registered = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 class FundingBody(models.Model):
+    # lookup table for Project
     name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Project(models.Model):
     funding_body = models.ForeignKey(FundingBody, on_delete=models.PROTECT)
@@ -45,33 +73,56 @@ class Project(models.Model):
     funding_number = models.CharField(max_length=50)
     funding_period_start = models.DateField()
     funding_period_end = models.DateField()
-    project_folder = models.CharField(max_length=255, null=True)
+    project_folder = models.CharField(max_length=255, null=True, blank=True)
     date_registered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class SampleType(models.Model):
+    # lookup table for Sample
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Sample(models.Model):
-    sample_id = models.CharField(max_length=20, validators=[MinLengthValidator(20)], primary_key=True)
-    institute = models.ForeignKey(Institute, on_delete=models.PROTECT)
-    method = models.ForeignKey(Method, on_delete=models.PROTECT, null=True)
-    project = models.ForeignKey(Project, on_delete=models.PROTECT)
-    sample_type = models.ForeignKey(SampleType, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    parent_id = models.CharField(max_length=20, validators=[MinLengthValidator(20)])
-    name = models.CharField(max_length=255)
-    date_created = models.DateField()
-    sample_info = models.CharField(max_length=255)
-    supplementary_file = models.CharField(max_length=255)
+    sample_id = models.CharField(max_length=20, validators=[MinLengthValidator(20)], primary_key=True,
+                help_text="a unique 20-character identifier for the sample")
+    institute = models.ForeignKey(Institute, on_delete=models.PROTECT, related_name="samples")
+    method = models.ForeignKey(Method, on_delete=models.PROTECT, null=True, blank=True,
+             related_name="samples")
+    project = models.ForeignKey(Project, on_delete=models.PROTECT, related_name="samples")
+    sample_type = models.ForeignKey(SampleType, on_delete=models.PROTECT, related_name="samples")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="samples")
+    parent = models.ForeignKey("self", on_delete=models.PROTECT, null=True, blank=True,
+             related_name="children")
+    name = models.CharField(max_length=255, help_text="free text field")
+    date_created = models.DateField(help_text="date of creation, rarely: delivery")
+    sample_info = models.CharField(max_length=255, help_text="link to sample type-specific information",
+                                   verbose_name="sample information file",)
+    # will probably have to change to FileField in the future for uploads
+    supplementary_file = models.CharField(max_length=255, null=True, blank=True,
+                         verbose_name="supplementary file path",
+                         help_text="if applicable: datasheet, etc.")
     date_registered = models.DateTimeField(auto_now_add=True)
-    
+
+    def __str__(self) -> str:
+        return self.name
+
 class Experiment(models.Model):
-    sample = models.ForeignKey(Sample, on_delete=models.PROTECT)
-    method = models.ForeignKey(Method, on_delete=models.PROTECT)
-    staff = models.ForeignKey(Staff, on_delete=models.PROTECT)
-    project = models.ForeignKey(Project, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    name = models.CharField(max_length=255)
-    date_created = models.DateField()
-    experiment_folder = models.CharField(max_length=255)
+    sample = models.ForeignKey(Sample, on_delete=models.PROTECT, related_name="experiments")
+    method = models.ForeignKey(Method, on_delete=models.PROTECT, related_name="experiments")
+    staff = models.ForeignKey(Staff, on_delete=models.PROTECT, related_name="experiments")
+    project = models.ForeignKey(Project, on_delete=models.PROTECT, related_name="experiments")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="experiments")
+    name = models.CharField(max_length=255, help_text="free text field")
+    date_created = models.DateField(help_text="when was the data created")
+    # all experimental data will have to be zipped, so single file
+    # will probably have to change to FileField in the future for uploads
+    experiment_file = models.CharField(max_length=255,
+                      verbose_name="experimental data file path",)
     date_registered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
