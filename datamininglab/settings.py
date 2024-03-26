30f3490ceb46a27b2f0f7313cdaacb8c06ac7b6c
Thomas Buchwald <thomas.buchwald@tu-freiberg.de>
2024-03-26 16:29:43 +0100

    prepared for initial push to remote

--- datamininglab/settings.py
+++ datamininglab/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.20.99.14', 'localhost', '127.0.0.1']
 
 # Application definition
 
