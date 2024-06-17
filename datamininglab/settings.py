163f9cbc482f7a3c7c7c647249d3b4b5d4121f84
Thomas Buchwald <thomas.buchwald@tu-freiberg.de>
2024-06-17 13:09:27 +0200

    minor typo fix

--- datamininglab/settings.py
+++ datamininglab/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = config("MEDIA_FOLDER")
 
 # Default primary key field type
